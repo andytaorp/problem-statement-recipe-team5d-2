162c0e11c54b33385cdfa7a1b947595467d4018e
shvqi <23031734@myrp.edu.sg>
2025-02-07 15:48:38 +0800

    Co-authored-by: 23040964-Danial <23040964-Danial@users.noreply.github.com> Co-authored-by: 23024637-Anaqy <23024637-Anaqy@users.noreply.github.com>

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { RecipesContextProvider } from './context/RecipesContext';
+import { AuthContextProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import RecipeForm from './components/RecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthContextProvider>
+        <RecipesContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<RecipeForm />} />
+            <Route path="/recipes/:id" element={<RecipeDetails />} />
+          </Routes>
+        </RecipesContextProvider>
+      </AuthContextProvider>
+    </Router>
   );
-}
+};
 
 export default App;
